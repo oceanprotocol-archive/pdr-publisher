4a07f885447cce0f793de964736253bb9e2e91dc
alexcos20 <alex.coseru@gmail.com>
2023-07-20 17:16:33 +0300

    more transfers

--- Dockerfile
+++ Dockerfile
@@ -20,7 +20,6 @@ RUN apt-get update && \
    git
 
 
-
 COPY . /pdr-publisher
 WORKDIR /pdr-publisher
 RUN git clone https://github.com/oceanprotocol/ocean.py
@@ -31,9 +30,6 @@ RUN pip install -r requirements_dev.txt
 RUN mkdir -p /root/.brownie/
 RUN cp /pdr-publisher/network-config.yaml /root/.brownie/
 ENV ADDRESS_FILE="${HOME}/.ocean/ocean-contracts/artifacts/address.json"
-ENV OPF_DEPLOYER_PRIVATE_KEY="0xc594c6e5def4bab63ac29eed19a134c130388f74f019bc74b8f4389df2837a58"
-ENV PREDICTOOR_PRIVATE_KEY="0xef4b441145c1d0f3b4bc6d61d29f5c6e502359481152f869247c7a4244d45209"
-ENV TRADER_PRIVATE_KEY="0x8467415bb2ba7c91084d932276214b11a3dd9bdb2930fefa194b666dd8020b99"
 ENV WAIT_FOR_CONTRACTS=false
 ENV PYTHONUNBUFFERED=1
 ENTRYPOINT ["/pdr-publisher/docker-entrypoint.sh"]
--- publish.py
+++ publish.py
@@ -8,9 +8,11 @@ from ocean_lib.ocean.util import from_wei, to_wei
 from ocean_lib.example_config import get_config_dict
 from ocean_lib.web3_internal.constants import ZERO_ADDRESS, MAX_UINT256
 # add accounts
+if "OPF_DEPLOYER_PRIVATE_KEY" not in os.environ:
+    print("Missing OPF_DEPLOYER_PRIVATE_KEY")
+    exit(1)
+
 deployer = br_accounts.add(os.getenv("OPF_DEPLOYER_PRIVATE_KEY"))
-predictoor = br_accounts.add(os.getenv("PREDICTOOR_PRIVATE_KEY"))
-trader = br_accounts.add(os.getenv("TRADER_PRIVATE_KEY"))
 connect_to_network("development")
 ADDRESS_FILE = "~/.ocean/ocean-contracts/artifacts/address.json"
 address_file = os.path.expanduser(ADDRESS_FILE)
@@ -21,14 +23,34 @@ ocean = Ocean(config)
 OCEAN = ocean.OCEAN_token
 
 # transfer ocean tokens to predictoor & trader
-print("Sending Ocean to predictoor")
-OCEAN.transfer(predictoor.address, to_wei(2000.0), {"from": deployer})
-print("Sending Ocean to trader")
-OCEAN.transfer(trader.address, to_wei(2000.0), {"from": deployer})
+if "PREDICTOOR_PRIVATE_KEY" in os.environ:
+    predictoor = br_accounts.add(os.getenv("PREDICTOOR_PRIVATE_KEY"))
+    print("Sending Ocean to predictoor")
+    OCEAN.transfer(predictoor.address, to_wei(2000.0), {"from": deployer})
+
+if "TRADER_PRIVATE_KEY" in os.environ:
+    trader = br_accounts.add(os.getenv("TRADER_PRIVATE_KEY"))
+    print("Sending Ocean to trader")
+    OCEAN.transfer(trader.address, to_wei(2000.0), {"from": deployer})
+
+if "DFBUYER_PRIVATE_KEY" in os.environ:
+    dfbuyer = br_accounts.add(os.getenv("DFBUYER_PRIVATE_KEY"))
+    print("Sending Ocean to dfbuyer")
+    OCEAN.transfer(dfbuyer.address, to_wei(10000.0), {"from": deployer})
+
+if "PDR_WEBSOCKET_KEY" in os.environ:
+    pdr_websocket_user = br_accounts.add(os.getenv("PDR_WEBSOCKET_KEY"))
+    print("Sending Ocean to pdr_websocket_user")
+    OCEAN.transfer(pdr_websocket_user.address, to_wei(10000.0), {"from": deployer})
+
+if "PDR_MM_USER" in os.environ:
+    pdr_mm_user = br_accounts.add(os.getenv("PDR_MM_USER"))
+    print("Sending Ocean to pdr_mm_user")
+    OCEAN.transfer(pdr_mm_user.address, to_wei(10000.0), {"from": deployer})
 
 #create NFT
 print("Creating NFT...")
-data_nft = ocean.data_nft_factory.create({"from": deployer}, "DN", "DN")
+
 S_PER_MIN = 60
 S_PER_HOUR = 60 * 60
 # for our ganache, have one epoch per minute (every 60 blocks)
@@ -39,9 +61,10 @@ min_predns_for_payout = 3  # ideally, 100+
 stake_token = OCEAN
 DT_price = 2
 
-#create template3
+#1st pair:  ETH-USDT on Kraken, 5m
+data_nft = ocean.data_nft_factory.create({"from": deployer}, "ETH-USDT-Kraken-5m", "ETH-USDT")
 initial_list = data_nft.getTokensList()
-print("Creating template3 datatoken...")
+print("Creating BTC-TUSD-Binance-5m...")
 data_nft.createERC20(
         3,
         ["ETH-USDT", "ETH-USDT"],
@@ -58,9 +81,62 @@ DT = DatatokenBase.get_typed(config, new_elements[0])
 print("Setting fixed price exchange")
 DT.setup_exchange({"from": deployer}, to_wei(DT_price))
 print("Setting key/values")
-data_nft.set_data("pair", "eth-usd", {"from": deployer})
+data_nft.set_data("pair", "eth-usdt", {"from": deployer})
 data_nft.set_data("base", "eth", {"from": deployer})
-data_nft.set_data("quote", "usd", {"from": deployer})
+data_nft.set_data("quote", "usdt", {"from": deployer})
 data_nft.set_data("source", "kraken", {"from": deployer})
+data_nft.set_data("timeframe", "5m", {"from": deployer})
+
+
+#2nd pair:  BTC-TUSD on Binance, 5m
+data_nft = ocean.data_nft_factory.create({"from": deployer}, "BTC-TUSD-Binance-5m", "BTC-TUSD")
+initial_list = data_nft.getTokensList()
+print("Creating BTC-TUSD-Binance-5m...")
+data_nft.createERC20(
+        3,
+        ["BTC-TUSD", "BTC-TUSD"],
+        [deployer.address, deployer.address, deployer.address, OCEAN.address, OCEAN.address],
+        [MAX_UINT256, 0, s_per_block, s_per_epoch, s_per_subscription, 30],
+        [],
+        {"from": deployer},
+    )
+new_elements = [
+        item for item in data_nft.getTokensList() if item not in initial_list
+    ]
+assert len(new_elements) == 1, "new datatoken has no address"
+DT = DatatokenBase.get_typed(config, new_elements[0])
+#print("Setting fixed price exchange")
+DT.setup_exchange({"from": deployer}, to_wei(DT_price))
+#print("Setting key/values")
+data_nft.set_data("pair", "btc-tusd", {"from": deployer})
+data_nft.set_data("base", "btc", {"from": deployer})
+data_nft.set_data("quote", "tusd", {"from": deployer})
+data_nft.set_data("source", "binance", {"from": deployer})
+data_nft.set_data("timeframe", "5m", {"from": deployer})
+
+#3rd pair:  XRP-USDT on Binance, 5m
+data_nft = ocean.data_nft_factory.create({"from": deployer}, "XRP-USDT-Binance-5m", "XRP-USDT")
+initial_list = data_nft.getTokensList()
+print("Creating XRP-USDT-Binance-5m...")
+data_nft.createERC20(
+        3,
+        ["XRP-USDT", "XRP-USDT"],
+        [deployer.address, deployer.address, deployer.address, OCEAN.address, OCEAN.address],
+        [MAX_UINT256, 0, s_per_block, s_per_epoch, s_per_subscription, 30],
+        [],
+        {"from": deployer},
+    )
+new_elements = [
+        item for item in data_nft.getTokensList() if item not in initial_list
+    ]
+assert len(new_elements) == 1, "new datatoken has no address"
+DT = DatatokenBase.get_typed(config, new_elements[0])
+#print("Setting fixed price exchange")
+DT.setup_exchange({"from": deployer}, to_wei(DT_price))
+#print("Setting key/values")
+data_nft.set_data("pair", "xrp-tusd", {"from": deployer})
+data_nft.set_data("base", "xrp", {"from": deployer})
+data_nft.set_data("quote", "usdt", {"from": deployer})
+data_nft.set_data("source", "binance", {"from": deployer})
 data_nft.set_data("timeframe", "5m", {"from": deployer})
 print("Done")
